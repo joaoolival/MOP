f6e97598b9b6f7c86a3ccc62627765706b312088
joaoolival <joaoljolival@gmail.com>
2015-04-02 18:18:21 +0100

    versao final P05Xadrez ja comentada

--- MOP/src/tps/tp1/pack3Arrays/P05Xadrez.java
+++ MOP/src/tps/tp1/pack3Arrays/P05Xadrez.java
@@ -10,33 +10,40 @@ public class P05Xadrez {
 		char[][] xadrez = new char[size][size];
 		// init random
 		Random random = new Random();
-		// atribuiçao das coordenadas aleatorias do bispo
+		// atribuiçao das coordenadas aleatorias da Torre1
 		int xTorre1 = random.nextInt(size);
 		int yTorre1 = random.nextInt(size);
 		xadrez[xTorre1][yTorre1] = 'T';
-
+		// init numero de ataques
+		int totalAttacks = 0;
+		// atribuiçao das coordenadas aleatorias do Bispo1
 		int xBispo1 = random.nextInt(size);
 		int yBispo1 = random.nextInt(size);
 		boolean bispo1 = false;
-
+		// ciclo para verificar se o bispo nao esta a ser atacado pela torre
 		while (!bispo1) {
+			// caso esteja a ser atacado
 			if ((yBispo1 == (yTorre1 + (xTorre1 - xBispo1)))
 					|| (yBispo1 == (yTorre1 + (xBispo1 - xTorre1)))
 					|| yBispo1 == yTorre1 || xBispo1 == xTorre1) {
+				// novas coordenadas random seram avaliadas
 				xBispo1 = random.nextInt(size);
 				yBispo1 = random.nextInt(size);
 			} else {
+				// caso nao esteja a ser atacado o programa prossegue
 				xadrez[xBispo1][yBispo1] = 'B';
 				bispo1 = true;
 			}
 
 		}
-
+		// coordenadas aleatorias para o Bispo2
 		int xBispo2 = random.nextInt(size);
 		int yBispo2 = random.nextInt(size);
 		boolean bispo2 = false;
-
+		// mesmo procedimento que foi feito para o bispo1
 		while (!bispo2) {
+			// neste caso e avaliado se o Bispo2 esta a ser atacado pela Torre1
+			// e pelo Bispo1
 			if ((yBispo2 == (yTorre1 + (xTorre1 - xBispo2)))
 					|| (yBispo2 == (yTorre1 + (xBispo2 - xTorre1)))
 					|| (yBispo2 == yTorre1 || xBispo2 == xTorre1)
@@ -45,6 +52,7 @@ public class P05Xadrez {
 				xBispo2 = random.nextInt(size);
 				yBispo2 = random.nextInt(size);
 			} else {
+				// caso nao, o progrmaa prossegue
 				xadrez[xBispo2][yBispo2] = 'B';
 				bispo2 = true;
 			}
@@ -53,8 +61,10 @@ public class P05Xadrez {
 		int xTorre2 = random.nextInt(size);
 		int yTorre2 = random.nextInt(size);
 		boolean torre2 = false;
-
+		// mesmo procedimento que foi feito para as outras peças
 		while (!torre2) {
+			// neste caso e avaliado se a Torre2 esta a ser atacada pela Torre1
+			// e pelo Bispo1 e Bispo2
 			if ((yBispo2 == (yTorre2 + (xTorre2 - xBispo2)))
 					|| (yBispo2 == (yTorre2 + (xBispo2 - xTorre2)))
 					|| (yBispo2 == yTorre2 || xBispo2 == xTorre2)
@@ -64,15 +74,65 @@ public class P05Xadrez {
 					|| (yTorre2 == yTorre1 || xTorre2 == xTorre1)) {
 				xTorre2 = random.nextInt(size);
 				yTorre2 = random.nextInt(size);
-			} else{
+			} else {
+				// caso nao, o progrmaa prossegue
 				xadrez[xTorre2][yTorre2] = 'T';
 				torre2 = true;
 			}
 		}
+		// ciclo para avaliar todas as casa vazias se tem ataques
+		for (int i = 0; i < xadrez.length; i++) {
+			for (int j = 0; j < xadrez[i].length; j++) {
+				// so serao avaliadas casas vazias
+				if (xadrez[i][j] != 'B' && xadrez[i][j] != 'T') {
+					// se for atacada pelo Bispo2
+					if ((yBispo2 == (j + (i - xBispo2)))
+							|| (yBispo2 == (j + (xBispo2 - i)))) {
+						// incrementa 1 na variavel de total de ataques
+						totalAttacks += 1;
+					}
+					// se for atacada pelo Bispo1
+					if ((yBispo1 == (j + (i - xBispo1)))
+							|| (yBispo1 == (j + (xBispo1 - i)))) {
+						// incrementa 1 na variavel de total de ataques
+						totalAttacks += 1;
+					}
+					// se for atacada pela Torre1
+					if ((j == yTorre1 || i == xTorre1)) {
+						// incrementa 1 na variavel de total de ataques
+						totalAttacks += 1;
+					}
+					// se for atacada pela Torre2
+					if ((j == yTorre2 || i == xTorre2)) {
+						// incrementa 1 na variavel de total de ataques
+						totalAttacks += 1;
+					}
+					// se o total de ataques for 0
+					if (totalAttacks == 0) {
+						// nessa casa aparecerá o char 'o'
+						xadrez[i][j] = 'o';
+					}
+					// se o total de ataques for 1
+					if (totalAttacks == 1) {
+						// nessa casa aparecerá o char '-'
+						xadrez[i][j] = '-';
+					}
+					// se o total de ataques for 2
+					if (totalAttacks >= 2) {
+						// nessa casa aparecerá o char '+'
+						xadrez[i][j] = '+';
+					}
+					// reinicio da varial totalAttacks
+					totalAttacks = 0;
+				}
+			}
+		}
 
-		for (int xXadrez = 0; xXadrez < xadrez.length; xXadrez++) {
-			for (int yXadrez = 0; yXadrez < xadrez[xXadrez].length; yXadrez++) {
-				System.out.print("[" + xadrez[xXadrez][yXadrez] + "]");
+		// x
+		for (int i = 0; i < xadrez.length; i++) {
+			// y
+			for (int j = 0; j < xadrez[i].length; j++) {
+				System.out.print("[" + xadrez[i][j] + "]");
 			}
 			System.out.println("");
 		}
