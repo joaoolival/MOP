10b802ccfa8d4854eeaacfb80a5dc03e787041bc
joaoolival <joaoljolival@gmail.com>
2015-04-29 22:47:15 +0100

    class test

--- MOP/src/tps/tp2/pack1Recursive/test.java
+++ MOP/src/tps/tp2/pack1Recursive/test.java
@@ -3,9 +3,16 @@ package tps.tp2.pack1Recursive;
 public class test {
 
 	public static void main(String[] args) {
-		int i = 359%10;
-		System.out.println(i);
+		char[] n = new char[] { 'a', 'c' };
+		n = copyOfRange(n,1,n.length);
+		
+		
 
+	}
+
+	private static char[] copyOfRange(char[] n, int i, int length) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
